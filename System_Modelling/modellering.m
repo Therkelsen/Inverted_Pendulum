cb8e41eff31dba0e543f869240b4cbdb77f71548
peterfryd <90767919+peterfryd@users.noreply.github.com>
2022-04-22 09:37:08 +0200

    fiks med modellering
    modellering er nu okay færdig

--- System_Modelling/modellering.m
+++ System_Modelling/modellering.m
@@ -2,25 +2,31 @@ clc, clear, close all
 sympref('AbbreviateOutput', false);
 syms theta(t) x(t)  u m M L g bcart bpend t I
 
+  %HER LAVES MODELERING VHA. LAGRANGE 
 
-% HER LAVES MODELERING VHA. LAGRANGE D'ALEMBERT
-
-Ekin = 0.5*(M+m)*diff(x, t)^2 + m*L*diff(x, t) * diff(theta, t)*cos(theta)+0.5*(m*L^2+I)*diff(theta, t)^2
+Ekin = 0.5*(M+m)*diff(x, t)^2 + m*L*diff(x, t) * diff(theta, t)*cos(theta)+0.5*(m*L^2 + I)*diff(theta, t)^2
 Epot = -m*g*L*cos(theta)
 
 Lagrange = Ekin - Epot
 
-% BEVÆGELSESLIGNINGERNE
+
+l1 = diff(diff(Lagrange, diff(x, t)), t) - diff(Lagrange, x) == - bcart*diff(x, t) + u
+l2 = diff(diff(Lagrange, diff(theta, t)), t) - diff(Lagrange, theta) == - bpend*diff(theta, t)
+
+ddx = isolate(l2, diff(x, t, t))
+ddt = isolate(l2, diff(theta, t, t))
+
+la = subs(l1, diff(x, t, t), rhs(ddx));
+eq2 = rhs(isolate(la, diff(theta, t, t)))
+
+lb = subs(l1, diff(theta, t, t), rhs(ddt));
+eq4 = rhs(isolate(lb, diff(x, t, t)))
+
 
 eq1 = diff(x, t)
-eq2 = diff(diff(Lagrange, diff(x, t)), t)-diff(Lagrange, x) == -bcart*diff(x, t) + u
+%eq2 = (m*L*sin(theta)*diff(theta, t)^2 - m*g*cos(theta)*sin(theta) - bcart*diff(x, t))/(M+m*sin(theta)^2) + u/(M+m*sin(theta)^2)
 eq3 = diff(theta, t)
-eq4 = diff(diff(Lagrange, diff(theta, t)), t)-diff(Lagrange, theta) == 0
-
-eq2 = (u + (m*(2*x + 2*L*cos(theta)*diff(theta, t)))/2 - bcart*diff(x, t))/M %isolate(eq2, diff(diff(x, t), t))
-eq4 = -(L*m*cos(theta)*diff(x, t) + L*g*m*sin(theta))/(I + L^2*m*cos(theta)^2 + L^2*m*sin(theta)^2)
- 
-%isolate(eq4, diff(diff(theta, t), t))
+%eq4 =  ((M+m)*g*sin(theta) + bcart*cos(theta)*diff(x, t) - m*L*sin(theta)*cos(theta)*diff(theta, t)^2)/(L*(M+m*sin(theta)^2))  - (cos(theta)*u)/(L*(M+m*sin(theta)^2))                             % -(L*m*cos(theta)*diff(x, t) + L*g*m*sin(theta))/(I + L^2*m*cos(theta)^2 + L^2*m*sin(theta)^2)  % == omega'
 
 
 % HER OPRETTES STATE-SPACE-VEKTORERNE
@@ -36,7 +42,7 @@ B_J = [diff(eq1, u); diff(eq2, u); diff(eq3, u); diff(eq4, u)]
 %HER UDFØRES LINEARISERING OMKRING PUNKTET (theta, diff(theta, t), diff(x, t)) = (pi, 0, 0)
 
 A = subs(A_J, {x, theta, diff(theta, t), diff(x, t), u}, {0, pi, 0, 0, 0}) % pi = linearisering omkring opretstående punkt
-B = subs(B_J, {x, theta, diff(theta, t), diff(x, t), u}, {0, pi, 0, 0, 0}) % 0 =linearisering omkring nedadstående punkt
+B = subs(B_J, {x, theta, diff(theta, t), diff(x, t), u}, {0, pi, 0, 0, 0})% 0 =linearisering omkring nedadstående punkt
 C = [1 0 0 0; 0 0 1 0] % vi måler x og theta (output) 
 D = [0; 0]
 
@@ -45,10 +51,10 @@ D = [0; 0]
 % HER ER NOGLE AF DE FYSISKE STØRRELSER FOR SYSTEMET
 
  mPendul = 0.084; % masse af pendul [kg]
- varM = 0.5; % masse af vogn [kg]
+ varM =   0.5; % masse af vogn [kg]
  mStang = 0.082;  % masse af stang [kg]
- varm = mStang + mPendul;
- varL = 0.35; % total længde af stang [m]
+ varm =  mStang + mPendul;
+ varL =  0.35; % total længde af stang [m]
  varg = 9.82; % tyngdeaccelerationen [m/s^2]
 % lConveyor = 1.72; %længde af conveyorbælte [m]
 % rollerRadius = 0.05; %radius af bælte-rullerne [m]
@@ -63,25 +69,37 @@ D = [0; 0]
 
 %DER INDSÆTTES NUMERISKE VÆRDIER HER
 
-A = double(subs(A, {m, M, L, g, bcart, I}, {varm, varM, varL, varg, varbcart, varI}))
-B = double(subs(B, {m, M, L, g, bcart, I}, {varm, varM, varL, varg, varbcart, varI}))
-C = double(subs(C, {m, M, L, g, bcart, I}, {varm, varM, varL, varg, varbcart, varI}))
-D = double(subs(D, {m, M, L, g, bcart, I}, {varm, varM, varL, varg, varbcart, varI}))
+A = double(subs(A, {m, M, L, g, bcart, bpend, I}, {varm, varM, varL, varg, varbcart, varbpend, varI}))
+B = double(subs(B, {m, M, L, g, bcart, bpend, I}, {varm, varM, varL, varg, varbcart, varbpend, varI}))
+C = double(subs(C, {m, M, L, g, bcart, bpend, I}, {varm, varM, varL, varg, varbcart, varbpend, varI}))
+D = double(subs(D, {m, M, L, g, bcart, bpend, I}, {varm, varM, varL, varg, varbcart, varbpend, varI}))
 
-q = (varM+varm)*(varI+varm*varL^2)-(varm*varL)^2
 
-a = varbcart*(varI+varm*varL^2)/q
-b = -varg*varL*varm
-%HER OPRETTES OVERFØRINGSFUNKTIONERNE
 s = tf('s');
 tf = C*(inv(s*eye(4, 4)-A))*B+D
 
-step(tf(2))
 
-[numP, denP] = tfdata(tf(2)) 
-[numC, denC] = tfdata(tf(1)) 
+%% HER OPRETTES OVERFØRINGSFUNKTIONERNE
 
-
+clear
 
 
+tfC = tf([3.957 78.24 26.69 1538 -119.5 7551  0], [1 39.58 386.4  283.1  7698  -1036 37750  0  0])
+tfP = tf([1.847 36.55 -4.924 358.8 0], [1 39.58 386.4  283.1  7698  -1036   37750])
+
+t = 0:0.01:10;
+%u = zeros(size(t));
+%u(t == 1) = 1;
+
+u = ones(size(t));
+
+plot(u)
+
+figure()
+lsim(tfP, u, t)
+title("Pendulum simulation")
+
+figure()
+lsim(tfC, u, t)
+title("Cart simulation")
 
